a1055476d90356e7fc485e389ae774c92060edc0
climatEch-sas <climatech947@gmail.com>
2026-04-10 20:22:47 -0500

    Update cotizacion.routes.js

--- src/routes/cotizacion.routes.js
+++ src/routes/cotizacion.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { getAll, getOne, create, updateEstado, remove, generatePDF, getSolicitudesByCliente } from '../controllers/cotizacion.controller.js';
+import { getAll, getOne, create, updateEstado, remove, generatePDF, getSolicitudesCliente } from '../controllers/cotizacion.controller.js';
 import { authenticate, authorize } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 router.use(authenticate);
 router.get('/', authorize('ADMIN', 'CLIENTE'), getAll);
-router.get('/solicitudes/:clienteId', authorize('ADMIN'), getSolicitudesByCliente);
+router.get('/solicitudes/:clienteId', authorize('ADMIN'), getSolicitudesCliente);
 router.get('/:id', authorize('ADMIN', 'CLIENTE'), getOne);
 router.get('/:id/pdf', authorize('ADMIN', 'CLIENTE'), generatePDF);
 router.post('/', authorize('ADMIN'), create);
